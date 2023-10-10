5cc6a1a2696768238405e3b633d9acbd3f01f9c7
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-10 21:31:56 +0200

    Set Up RadixUI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
