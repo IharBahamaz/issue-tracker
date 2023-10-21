3aaadf2ae6754b2085877fdcdc65054444095c21
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-21 23:35:17 +0200

    Add a delete button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,7 +1,7 @@
-import { IssueStatusBadge } from '@/app/components';
-import { Issue } from '@prisma/client';
-import { Heading, Flex, Card, Text } from '@radix-ui/themes';
-import ReactMarkdown from 'react-markdown';
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
   return (
@@ -11,7 +11,7 @@ const IssueDetails = ({ issue }: { issue: Issue }) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card className="prose" mt="4">
+      <Card className="prose max-w-full" mt="4">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </>
