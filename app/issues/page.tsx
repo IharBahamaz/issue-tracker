481af16f7e48c1897b28329967de3e268e816254
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-21 19:17:17 +0200

    Fixing Cache Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
-import prisma from '@/prisma/client';
-import { Table } from '@radix-ui/themes';
-import { IssueStatusBadge, Link } from '../components';
-import IssueActions from './IssueActions';
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "../components";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -43,5 +43,6 @@ const IssuesPage = async () => {
     </div>
   );
 };
+export const dynamic = "force-dynamic";
 
 export default IssuesPage;
