f14d42ebb19d0e33f9abb93530dfb97226ed6d89
IllusoryedoC <codeoffantasy@gmail.com>
2023-10-24 23:48:31 +0200

    Finally find bug in Select Trigger

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -9,6 +9,7 @@ import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const { data: users, error, isLoading } = useUsers();
+  console.log('Users data ', users);
 
   if (isLoading) return <Skeleton />;
 
@@ -27,22 +28,21 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   return (
     <>
       <Select.Root
-        defaultValue={issue.assignedToUserId || ''}
+        // defaultValue={issue.assignedToUserId || ''}
         onValueChange={assignIssue}
       >
-        <Select.Trigger placeholder='Assign...'>
-          <Select.Content>
-            <Select.Group>
-              <Select.Label>Suggestion</Select.Label>
-              <Select.Item value=''>Unassigned</Select.Item>
-              {users?.map((user) => (
-                <Select.Item key={user.id} value={user.id}>
-                  {user.name}
-                </Select.Item>
-              ))}
-            </Select.Group>
-          </Select.Content>
-        </Select.Trigger>
+        <Select.Trigger placeholder='Assign...' />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value=''>Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
       </Select.Root>
       <Toaster />
     </>
